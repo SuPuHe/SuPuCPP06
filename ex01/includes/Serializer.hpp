72eca61bf7e84082230de48936648054e9029a4b
SuPuHe <mizin.a.s.2006@gmail.com>
2025-12-09 16:24:52 +0100

    init commit of ex02

---
+++ ex01/includes/Serializer.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serializer.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omizin <omizin@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/09 13:55:04 by omizin            #+#    #+#             */
+/*   Updated: 2025/12/09 16:23:20 by omizin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+struct Data
+{
+	int			number;
+	std::string	str;
+};
+
+class Serializer
+{
+	private:
+		Serializer();
+		Serializer(Serializer const &copy);
+		Serializer &operator=(Serializer const &copy);
+		~Serializer();
+	public:
+		static uintptr_t	serialize(Data* ptr);
+
+		static Data*		deserialize(uintptr_t raw);
+};
